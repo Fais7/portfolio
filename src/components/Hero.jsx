09e8f1b4cc24ae84ff3846af4a47b67719803a26
faisal farhan <faisal7farhan@gmail.com>
2021-12-07 13:00:24 +0530

    img added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -44,7 +44,7 @@ function Hero() {
           <ScrollLink>
             Scroll down
             <img
-              src={"https://raw.githubusercontent.com/gurupawar/portfolio/main/assets/scroll-down.svg"}
+              src={"https://raw.githubusercontent.com/Fais7/portfolio/main/src/Assets/scroll-down.svg"}
               alt="scroll-down"
             />
           </ScrollLink>
