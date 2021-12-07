a0386c1c68de63976829e04af70c503e5ead1b15
faisal farhan <faisal7farhan@gmail.com>
2021-12-07 13:31:58 +0530

    some changes

--- src/data/ProjectData.jsx
+++ src/data/ProjectData.jsx
@@ -13,74 +13,73 @@ import pro4 from '../Assets/pro4.png'
 
 // --------------------------------------- Project 
 export const ProjectList = [
-    
-    {
-      img: pro3,
-      title: "List of all the NGO's",
-      description: "Designed complete frontend using bootstrap, html, external css .",
-      tech_stack: "HTML, Css, bootStrap.",
-      github_url: "https://www.donationfornations.com/"
-    },
-    {
-      img: pro4,
-      title: "Donations",
-      description: "Designed complete frontend using bootstrap, html, internal css .",
-      tech_stack: "HTML, Css, bootStrap.",
-      github_url: "https://www.donatable.in/"
-    },
-    {
-      img: pro1,
-      title: "Text playGround",
-      description: "It will make your text easy. you can make uppercase,lowercase,firstletter capital etc..",
-      tech_stack: "HTML, Reactjs(functional based), bootStrap.",
-      github_url: "https://fais7.github.io/txtpground/"
-      
-    },
-    {
-      img: pro2,
-      title: "Ecommerce website (underdevelopment)",
-      description: "Created simple ecommerce website using reactjs, routing and axios(rest api) .",
-      tech_stack: "Reactjs(functional based), HTML, Css, bootStrap.",
-      github_url: "https://fais7.github.io/admincom/"
-    },
-  ];
-    
-  export const stackList = [
-    {
-      img: htmll,
-      name: "HTML, CSS",
-    },
-    {
-      img: csss,
-      name: "CSS",
-    },
-    {
-      img: js,
-      name: "JavaScript",
-    },
-    {
-      img: bootstrap,
-      name: "Bootstrap",
-    },
-    {
-      img: java,
-      name: "javaCore",
-    },
-    {
-      img: java,
-      name: "javaAdvance",
-    },
-    {
-      img: react,
-      name: "ReactJS",
-    },
-    {
-      img: mysql,
-      name: "mySQL",
-    },
-    {
-      img: git,
-      name: "Git",
-    },
-  ];
-  
+
+  {
+    img: pro1,
+    title: "Text playGround",
+    description: "It will make your text easy. you can make uppercase,lowercase,firstletter capital etc..",
+    tech_stack: "HTML, Reactjs(functional based), bootStrap.",
+    github_url: "https://fais7.github.io/txtpground/"
+
+  },
+  {
+    img: pro4,
+    title: "Donations",
+    description: "Designed complete frontend using bootstrap, html, internal css .",
+    tech_stack: "HTML, Css, bootStrap.",
+    github_url: "https://www.donatable.in/"
+  },
+  {
+    img: pro2,
+    title: "Ecommerce website (underdevelopment)",
+    description: "Created simple ecommerce website using reactjs, routing and axios(rest api) .",
+    tech_stack: "Reactjs(functional based), HTML, Css, bootStrap.",
+    github_url: "https://fais7.github.io/admincom/"
+  },
+  {
+    img: pro3,
+    title: "List of all the NGO's",
+    description: "Designed complete frontend using bootstrap, html, external css .",
+    tech_stack: "HTML, Css, bootStrap.",
+    github_url: "https://www.donationfornations.com/"
+  },
+];
+
+export const stackList = [
+  {
+    img: htmll,
+    name: "HTML, CSS",
+  },
+  {
+    img: csss,
+    name: "CSS",
+  },
+  {
+    img: js,
+    name: "JavaScript",
+  },
+  {
+    img: bootstrap,
+    name: "Bootstrap",
+  },
+  {
+    img: java,
+    name: "javaCore",
+  },
+  {
+    img: java,
+    name: "javaAdvance",
+  },
+  {
+    img: react,
+    name: "ReactJS",
+  },
+  {
+    img: mysql,
+    name: "mySQL",
+  },
+  {
+    img: git,
+    name: "Git",
+  },
+];
